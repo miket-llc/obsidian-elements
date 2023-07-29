23c4c3c0ca9c0d9c1b21758a1ec6570d2fac9600
miket-llc <mdt_@msn.com>
2023-07-29 13:47:09 -0400

    Implemented settings for folder paths.

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface ElementsPluginSettings {
-	mySetting: string;
+	homePath: string, 
+	mapsPath: string, 
+	conceptsPath: string, 
+	peoplePath: string,
+	meetingsPath: string,
+	projectsPath: string,
 }
 
 const DEFAULT_SETTINGS: ElementsPluginSettings = {
-	mySetting: 'default'
+	homePath: '/00 Home',
+	mapsPath: '/01 Maps',
+	conceptsPath: '/02 Concepts',
+	peoplePath: '/03 People',
+	meetingsPath: '/04 Meetings',
+	projectsPath: '/05 Projects',
 }
 
 export default class ElementsPlugin extends Plugin {
@@ -16,8 +26,8 @@ export default class ElementsPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Elements Plugin', (evt: MouseEvent) => {
+		// This creates an icon in the left ribbon. See https://lucide.dev/icons. 
+		const ribbonIconEl = this.addRibbonIcon('atom', 'Elements Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
@@ -27,6 +37,8 @@ export default class ElementsPlugin extends Plugin {
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Elements is active.');
+
+		// Create a command to automatically sort all files in the vault to the correct folders
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -66,7 +78,7 @@ export default class ElementsPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new ElementsSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -107,7 +119,7 @@ class ElementsSampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class ElementsSettingTab extends PluginSettingTab {
 	plugin: ElementsPlugin;
 
 	constructor(app: App, plugin: ElementsPlugin) {
@@ -119,15 +131,71 @@ class SampleSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
+		containerEl.createEl('h2', {text: "Notes Folders"})
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Home Folder')
+			.setDesc('Folder for a start page or readme.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Folder name')
+				.setValue(this.plugin.settings.homePath)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.homePath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Maps Folder')
+			.setDesc('Folder for a start page or readme.')
+			.addText(text => text
+				.setPlaceholder('Folder name')
+				.setValue(this.plugin.settings.mapsPath)
+				.onChange(async (value) => {
+					this.plugin.settings.homePath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Concepts Folder')
+			.setDesc('Concepts Folder.')
+			.addText(text => text
+				.setPlaceholder('Path to folder')
+				.setValue(this.plugin.settings.conceptsPath)
+				.onChange(async (value) => {
+					this.plugin.settings.peoplePath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+				.setName('People Folder')
+				.setDesc('Folder to store Person notes.')
+				.addText(text => text
+					.setPlaceholder('Path to folder')
+					.setValue(this.plugin.settings.peoplePath)
+					.onChange(async (value) => {
+						this.plugin.settings.peoplePath = value;
+						await this.plugin.saveSettings();
+					}));
+
+		new Setting(containerEl)
+			.setName('Meetings Folder')
+			.setDesc('Folder for Meeting notes.')
+			.addText(text => text
+				.setPlaceholder('Path to folder')
+				.setValue(this.plugin.settings.meetingsPath)
+				.onChange(async (value) => {
+					this.plugin.settings.meetingsPath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Projects Folder')
+			.setDesc('Folder for Project Notes.')
+			.addText(text => text
+				.setPlaceholder('Path to folder')
+				.setValue(this.plugin.settings.projectsPath)
+				.onChange(async (value) => {
+					this.plugin.settings.projectsPath = value;
 					await this.plugin.saveSettings();
 				}));
 	}

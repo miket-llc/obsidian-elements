ac642b1b23128f5f59a386cdc05ed6b951b4f75d
miket-llc <mdt_@msn.com>
2023-08-06 20:29:10 -0400

    Added addFileTransport method and monads.

--- src/lib/logger/logger.ts
+++ src/lib/logger/logger.ts
@@ -1,4 +1,5 @@
 import { createLogger, format, transports } from 'winston'
+import { IResult, ok, fail, maybe} from 'typescript-monads'
 
 export type logLevel = 'error' | 'warn' | 'info' | 'verbose' | 'debug' | 'silly'
 
@@ -7,18 +8,38 @@ export function log( level: logLevel, message: string ) {
 }
 
 export default class Logger {
+    private static _logLevel: logLevel = 'info'
+    private static _consoleTranport = new transports.Console();
+    private static _fileTransportMap = new Map<string, transports.FileTransportInstance>()
     private static _logger = createLogger({
-            level: 'info',
-            transports: [new transports.Console()],
-            format: format.combine(
-                format.colorize({level: true}),
-                format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-                format.printf(info => `${info.level}: ${info.message}`),
-            )
-       })
+        level: this._logLevel,
+        transports: [Logger._consoleTranport],
+        format: format.combine(
+            format.colorize({level: true}),
+            format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+            format.printf(info => `${info.level}: ${info.message}`),
+        )
+    })
+
+    static get consoleLevel(): logLevel { return Logger._consoleTranport.level as logLevel }
+    static set consoleLevel(level: logLevel) { Logger._consoleTranport.level = level }
+
+    static addFileTransport( filename: string, level: logLevel ): IResult<boolean, Error> {
+        try {
+            if(!Logger._fileTransportMap.has(filename)) {
+                const fileTransport = new transports.File({ filename, level })
+                Logger._fileTransportMap.set(filename, fileTransport)
+                Logger._logger.add(fileTransport)
+                return ok(true)
+            } else {
+                return fail(new Error(`File transport already exists for ${filename}`))
+            }
+        } catch(e) {
+            return fail(new Error(`Error adding file transport for ${filename}: ${maybe(e.message).valueOr('unknown error')}`))
+        }
+    }
 
     static log( level: logLevel, message: string ) {
        Logger._logger.log(level, message)
     }
 }
-        

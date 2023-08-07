f04f74115943aff69694781dcafce4fd4ff5d8dd
miket-llc <mdt_@msn.com>
2023-08-07 13:20:40 -0400

    Deleted unecessarily redundant abstraction.

--- src/ui/notice.ts
+++ src/ui/notice.ts
@@ -1,18 +1,7 @@
 import { Notice } from "obsidian";
-import { ElementsError } from "../utils/Error";
 
 export function logMessage(msg: string): void {
     const notice = new Notice("", 15000);
     notice.setMessage(`<b>Elements</b>:<br/>`+ msg);
     console.log(msg);
-}
-
-export function logError(e: Error | ElementsError): void {
-    const notice = new Notice("", 15000);
-    if (e instanceof ElementsError && e.console_msg) {
-        notice.setMessage(`<b>Elements Error</b>:<br/>' + e.message + '<br/>Check console for more information`);
-        console.error(`Elements Error:`, e.message, "\n", e.console_msg);
-    } else {
-        notice.noticeEl.innerHTML = `<b>Elements Error</b>:<br/>${e.message}`;
-    }
 }

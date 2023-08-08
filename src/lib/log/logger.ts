31d3af77dc303fac5229bf6416da4a578f82cb92
miket-llc <mdt_@msn.com>
2023-08-08 10:47:25 -0400

    Updated documentation.

--- src/lib/log/logger.ts
+++ src/lib/log/logger.ts
@@ -1,5 +1,6 @@
 import { createLogger, format, transports } from 'winston'
 
+// Levels above which a log will log messages. 
 export type loggerLevel = 'error' | 'warn' | 'info' | 'verbose' | 'debug' | 'silly'
 
 const _consoleTranport = new transports.Console();
@@ -14,18 +15,28 @@ const _logger = createLogger({
     )
 })
 
+/**
+ * Returns the current log level of the default console log. All levels above this level are being logged.
+ */
 export function getConsoleLevel(): loggerLevel { return _consoleTranport.level as loggerLevel }
+
+/**
+ * Sets the log level of the default console log. All levels above this level are being logged.
+ * @param level @link{logLevel} to which to set the default console log.
+ */
 export function setConsoleLevel(level: loggerLevel) { _consoleTranport.level = level }
 
 /**
- * Adds a new file transport to the logger. You are responsible for ensuring that the path and filename are valid.
+ * Adds a new file transport to the logger and sets its @link(logLevel). 
+ * You are responsible for ensuring that the path and filename are valid. Errors are not thrown, but
+ * are reported via callbacks registered with the @link(onError) method.
  * @param filename Path and filename of the log file to add.
  * @param level @link{logLevel} filter for this file. All levels above this level will be logged.
  * @warn If a file transport with the same filename has already been added, this method will do nothing.
  * @warn By design, this api will not throw errors for file errors. We highly recommend registering a
  * callback witn the link @link(onError) method to handle file errors.
  */
-export function addFileLog( filename: string, level: loggerLevel ): boolean {
+export function addFileTransport( filename: string, level: loggerLevel ): boolean {
     if(!_fileTransportMap.has(filename)) {
         const fileTransport = new transports.File({ filename, level })
         _fileTransportMap.set(filename, fileTransport)
@@ -39,15 +50,15 @@ export function addFileLog( filename: string, level: loggerLevel ): boolean {
  * @param filename Path and filename of the log file previously added.
  * @returns True if the file transport was removed, false if it was not found.
  */
-export function removeFileLog( filename: string ): boolean {
+export function removeFileTransport( filename: string ): boolean {
     return _logger.remove(_fileTransportMap.get(filename)) !== undefined
 }
 
 /**
- * Safely loges a message at the provided @type{loggerLevel} to all active log transports. Note that, by design, exactly one
+ * Safely logs a message at the provided @type{loggerLevel} to all active log transports. Note that, by design, exactly one
  * console is transport is always active. Each transport has it's own log level filter.
  * @param level Level of this message. If the @type(logLevel) is below the level of the transport, the message will not be logged.
- * @param message Message to add to each log transport.
+ * @param message Message to be sent to all active log transports.
  */
 export function log( level: loggerLevel, message: string ) {
     _logger.log(level, message)
